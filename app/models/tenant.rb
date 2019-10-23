4c7f1fde2321e4a001efac42157979c0d3e8e5f6
Ray Amable <rayamable@gmail.com>
2019-10-23 07:00:29 +0000

    add plans for tenants

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,17 +1,19 @@
 class Tenant < ApplicationRecord
 
-   acts_as_universal_and_determines_tenant
-  has_many :members, dependent: :destroy
+ acts_as_universal_and_determines_tenant
+ has_many :members, dependent: :destroy
+ validates_presence_of :name
+ validates_uniqueness_of :name
 
-    def self.create_new_tenant(tenant_params, user_params, coupon_params)
+ def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
-      tenant = Tenant.new(:name => tenant_params[:name])
+   tenant = Tenant.new(tenant_params)
 
-      if new_signups_not_permitted?(coupon_params)
+   if new_signups_not_permitted?(coupon_params)
 
-        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
+    raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
 
-      else 
+  else 
         tenant.save    # create the tenant
       end
       return tenant
@@ -35,7 +37,7 @@ class Tenant < ApplicationRecord
   #   tenant -- new tenant obj
   #   other  -- any other parameter string from initial request
   # ------------------------------------------------------------------------
-    def self.tenant_signup(user, tenant, other = nil)
+  def self.tenant_signup(user, tenant, other = nil)
       #  StartupJob.queue_startup( tenant, user, other )
       # any special seeding required for a new organizational tenant
       #
@@ -43,5 +45,5 @@ class Tenant < ApplicationRecord
       #
     end
 
-   
-end
+
+  end
--- config/initializers/milia.rb
+++ config/initializers/milia.rb
@@ -5,16 +5,16 @@ Milia.setup do |config|
 
 # true if you wish to use a coupon-based option in your sign-up form
 # false to otherwise not expect a coupon attribute in the parameters
-  config.use_coupon = false
+config.use_coupon = false
 
 # true if you use recaptcha on your sign-up form;
   # be sure to include the gem 'recaptcha' in your Gemfile
 # false if you do not
-  config.use_recaptcha = false
+config.use_recaptcha = false
 
 # true if action after signing out is to return to the home (root) page
 # false to return to the sign-in form (devise default)
-  config.signout_to_root = true
+config.signout_to_root = true
 
 # true if you are using airbrake to be notified of exceptions
   # be sure to include the gem 'airbrake' in your Gemfile
@@ -28,13 +28,13 @@ Milia.setup do |config|
 
 # use invite_member for devise work-around to invite members
 # ASSUMES User model
-  config.use_invite_member = true
+config.use_invite_member = true
 
   # whitelist tenant params list
   # allows an app to expand the permitted attribute list
   # specify each attribute as a symbol
   # example: [:name]
-  # config.whitelist_tenant_params = []
+  config.whitelist_tenant_params = [:plan]
 
   # whitelist coupon params list
   # allows an app to expand the permitted attribute list
