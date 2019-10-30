475616fc2940500e3e2bf150daeff2bcc27e5dc5
Ray Amable <rayamable@gmail.com>
2019-10-30 14:36:22 +0000

    complete artifacts

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,6 @@
 class Artifact < ApplicationRecord
+
+	before_save :upload_to_s3
 	attr_accessor :upload
 	belongs_to :project
 
@@ -7,6 +9,16 @@ class Artifact < ApplicationRecord
 	validates_uniqueness_of :name
 
 	validate :uploaded_file_size
+
+	private 
+
+	def upload_to_s3
+		s3 = Aws::S3::Resource.new
+		tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+		obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+		obj.upload_file(upload.path, acl:'public-read')
+		self.key = obj.public_url
+	end
 
 	def uploaded_file_size
 		if upload
