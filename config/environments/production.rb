c26acd160a593c006af855587f2a2171562ecd54
Ray Amable <rayamable@gmail.com>
2019-11-09 13:23:04 +0000

    add images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-   
+ 
   # devise says to define default url
   config.action_mailer.default_url_options = { :host => 'secure.simple-milia-app.com', :protocol => 'https' }
 
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.default_url_options = { :host => 'https://raysaasapp.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'https://projectsaas.herokuapp.com/', :protocol => 'https'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
